769977d135119515b230a83e0554a3b4e15236d3
Kyle Stevenson <kyle@kylestevenson.me>
2019-06-03 11:38:56 -0700

    Switch to hash for query

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@ export default {
   },
   methods: {
     parseLocation () {
-      const params = new URLSearchParams(location.search)
+      const params = new URLSearchParams(location.hash)
       const tile = params.get('tile') || '3223,3220,1'
       const [x, y, z] = tile.split(',').map(x => parseInt(x))
 
